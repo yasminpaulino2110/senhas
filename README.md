65eaa088dad81a2455e90365362bfcd07108b682
yasminpaulino2110 <paulino.silva.yasmin@escola.pr.gov.br>
2026-06-18 07:54:37 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# senhas
+# Gerador-de-Senhas-Alura
+
+Projeto do curso da Alura para aula de programação da terceira série do ensino médio de 2025/2026.
+
+Aluno faça donwload do código, altere as cores e coloque no seu GitHub.
---
+++ main.js
@@ -0,0 +1,98 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        //tamanhoSenha = tamanhoSenha - 1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+    //console.log(i);
+}
+
+
+geraSenha();
+function geraSenha() {
+    let alfabeto = '';
+
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+
+    //console.log(alfabeto);
+
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+        //console.log(letrasMaiusculas[numeroAleatorio]);
+    }
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+    console.log(alfabeto.length);
+
+    function exibirAlerta() {
+        if (alfabeto.length == 0) {
+            alert('Escolha pelo menos um dos campos.');
+        }
+    }
+    exibirAlerta();
+}
+
+
+function classificaSenha(tamanhoAlfabeto) {
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (entropia > 57) {
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35) {
+        forcaSenha.classList.add('fraca');
+    }
+
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + ' dias para um computador quebrar sua senha!';
+    function senhaFraca() {
+        if (valorEntropia.textContent == 0 + ' dias para um computador quebrar sua senha!') {
+            valorEntropia.textContent = 'Senha extremamente fraca, sua segurança está em risco.'
+        }
+    }
+    senhaFraca();
+}
